a6e036ecf44d8de49068e6dc058030ed0d7a7a9c
evertonleonel <everton_leonel@hotmail.com>
2022-11-07 10:21:43 -0300

    Estrutura inicial

---
+++ src/Components/GlobalStyles/styles.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-size: 1.6rem;
+    font-family: 'Roboto', sans-serif;
+    background-color: var(--cor-cinza-fraco);
+  }
+`
+
+
+
---
+++ src/Components/Header/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 24px 36px;
+  width: 100%;
+  max-height: 96px;
+  background-color: #FFFFFF;
+`
